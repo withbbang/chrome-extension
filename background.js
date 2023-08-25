ca7fe37fbc1011e90bd0e1fc51c9ff9beeb9df9b
김영선 <withbbang@gmail.com>
2023-08-25 17:30:18 +0900

    Refactor: array method로 변수 선언 최소화, 코드 설명 주석 추가

--- background.js
+++ background.js
@@ -60,21 +60,23 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       if (res.status === 200) {
         res.text().then((text) => {
           const parser = new DOMParser();
-          const daumDocument = parser.parseFromString(text, "text/html");
-          const titleWordTags = daumDocument.querySelectorAll(".tit_word");
-          const searchResults = [...titleWordTags]
+          const searchResults = [
+            ...parser
+              .parseFromString(text, "text/html")
+              .querySelectorAll(".tit_word"),
+          ] // 돔 파싱 후, 언어 사전 타이틀 읽어오기
             .filter(
               (titleWordTag) =>
                 titleWordTag.innerText === "한국어사전" ||
                 titleWordTag.innerText === "영어사전" ||
                 titleWordTag.innerText === "영영사전"
-            )
+            ) // 특정 언어 사전만 걸러내기
             .sort((a, b) => {
               if (dict[a.innerText] < dict[b.innerText]) return -1;
               else return 1;
-            })
+            }) // 순서 설정 -> 한국어, 영어, 영영
             .map((tag) => tag.parentElement)
-            .map((tag) => tag.nextElementSibling);
+            .map((tag) => tag.nextElementSibling); // 뜻 담고있는 부모 태그 리스트 접근
 
           if (
             searchResults &&
@@ -91,7 +93,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
               [...li.getElementsByClassName("txt_search")]
                 .map((txt) => txt.innerHTML.replace(/(<([^>]+)>)/gi, ""))
                 .join("")
-            );
+            ); // 뜻만 추출하여 리스트업
 
             sendResponse({
               status: res.status,
